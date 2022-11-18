768096d1f0625cbf568877045a56704e53276c24
Han Le <hoanghan.le.95@gmail.com>
2022-11-18 10:21:48 +0700

    Multi env with flavor (android setting)

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:base_project_getx/app/core/utils/app_log.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+
+import 'app/core/themes/app_theme.dart';
+import 'app/core/utils/app_config.dart';
+import 'app/routes/app_pages.dart';
+
+Future<void> main() async {
+  /// Start services later
+  WidgetsFlutterBinding.ensureInitialized();
+
+  /// Force portrait mode
+  await SystemChrome.setPreferredOrientations(
+      <DeviceOrientation>[DeviceOrientation.portraitUp]);
+
+  /// Load environment and set app config
+  await _loadEnvironment();
+
+  /// Run Application
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Application',
+      debugShowCheckedModeBanner: false,
+      initialRoute: AppPages.INITIAL,
+      getPages: AppPages.routes,
+      theme: AppTheme().light,
+    );
+  }
+}
+
+Future<void> _loadEnvironment() async {
+  /// Get Flavor value from native via method channel
+  String? flavor =
+      await const MethodChannel('flavor').invokeMethod<String>('getFlavor');
+
+  if (flavor == 'development') {
+    AppConfig(env: Env.dev(), theme: AppTheme());
+    logger.i('Started with flavor $flavor!');
+  } else if (flavor == 'production') {
+    AppConfig(env: Env.prod(), theme: AppTheme());
+    logger.i('Started with flavor $flavor!');
+  } else {
+    logger.e('Unknown flavor: $flavor');
+    throw Exception("Unknown flavor: $flavor");
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,7 +5,7 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:base_project_getx/my_app.dart';
+import 'package:base_project_getx/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
